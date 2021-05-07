d43f5fec7c2546200ed588297bc117e4825f6abe
Sousa Silva Engenharia de Sistemas <rs.ronilsonsilva@outlook.com.br>
2021-05-07 19:03:03 -0300

    a

--- src/services/SmartBank/SmartBankUsers.Application/ViewModels/ClienteViewModel.cs
+++ src/services/SmartBank/SmartBankUsers.Application/ViewModels/ClienteViewModel.cs
@@ -9,13 +9,13 @@ namespace SmartBank.Application.ViewModels
         public string Cpf { get; set; }
         public string Rg { get; set; }
         public string Cnh { get; set; }
-        public DateTime DataNascimento { get; set; }
-        public SexoPessoa Sexo { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public SexoPessoa? Sexo { get; set; }
         public ContatoValueObject Contato { get; set; }
         public EnderecoValueObject Endereco { get; set; }
         public string NomeMae { get; set; }
         public string NomePai { get; set; }
-        public GrauEscolaridade Escolaridade { get; set; }
+        public GrauEscolaridade? Escolaridade { get; set; }
         public ClienteEmpresaValueObject EmpresaTrabalho { get; set; }
         public string Usuario { get; set; }
         public string Password { get; set; }
