6230b7e6f67724a9d289ef99f318376fcabc1af9
Ronilson de Sousa Silva <ronilson@sousasilva.eng.br>
2021-05-11 16:59:48 -0300

    asdfadsfads

---
+++ src/services/SmartBankDataValid/SmartBank.DataValid.api/Configurations/ApiConfig.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using SmartBank.DataValid.Api.Extensions;
+
+namespace SmartBank.DataValid.Api.Configurations
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(options => options.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects);
+
+            #region [Configurações do IdentityServer]
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(o =>
+                {
+                    o.Authority = "https://sso.ronilson.dev";
+                    o.Audience = "api_cliente_smart_bank";
+                    o.RequireHttpsMetadata = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                //options.AddPolicy("ClientIdPolicy", policy => policy.RequireClaim("client_id", "movieClient", "movies_mvc_client"));
+            });
+
+            #endregion
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            //app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/services/SmartBankDataValid/SmartBank.DataValid.api/Configurations/SwaggerConfig.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace SmartBank.DataValid.Api.Configurations
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "SmartBank API Obras",
+                    Version = "1.0",
+                    Description = "API para gerenciamento de clientes",
+                    Contact = new OpenApiContact() { Name = "Ronilson Silva", Email = "ronilson@sousasilva.eng.br" },
+                    License = new OpenApiLicense() { Name = "Privada", Url = new Uri("http://sousasilva.eng.br/") }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "API - SmartObras V0.0.001");
+            });
+
+            return app;
+        }
+    }
+}
